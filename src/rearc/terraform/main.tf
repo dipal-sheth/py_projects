a584d9ec0cb58f4c88661657a5ef7e8469279524
Dipal <dip2002@gmail.com>
2025-06-27 01:18:41 -0500

    terra

--- src/rearc/terraform/main.tf
+++ src/rearc/terraform/main.tf
@@ -6,6 +6,12 @@ resource "aws_sqs_queue" "s3_event_queue" {
   name = "dipal-sqs-queue"
 }
 
+resource "aws_s3_bucket" "data_bucket" {
+  bucket = "dipal-projects-bucket"
+  force_destroy = true
+}
+
+
 resource "aws_iam_role" "lambda_exec_role" {
   name = "dipal-lambda-role"
   assume_role_policy = jsonencode({
@@ -19,6 +25,25 @@ resource "aws_iam_role" "lambda_exec_role" {
     }]
   })
 }
+
+resource "aws_iam_role_policy" "lambda_sqs_permissions" {
+  name = "lambda-sqs-access"
+  role = aws_iam_role.lambda_exec_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Action = [
+        "sqs:ReceiveMessage",
+        "sqs:DeleteMessage",
+        "sqs:GetQueueAttributes"
+      ],
+      Resource = aws_sqs_queue.s3_event_queue.arn
+    }]
+  })
+}
+
 
 resource "aws_iam_policy_attachment" "lambda_basic_exec" {
   name       = "dipal-basic-exec"
@@ -36,7 +61,7 @@ resource "aws_lambda_function" "lambda_a" {
   timeout          = 10
   environment {
     variables = {
-      BUCKET_NAME = "dipal-projects-bucket"
+      BUCKET_NAME = aws_s3_bucket.data_bucket.bucket
     }
   }
 }
@@ -58,7 +83,7 @@ resource "aws_lambda_event_source_mapping" "lambda_b_sqs_trigger" {
 }
 
 resource "aws_s3_bucket_notification" "s3_to_sqs" {
-  bucket = "dipal-projects-bucket"
+  bucket = aws_s3_bucket.data_bucket.id
 
   queue {
     queue_arn = aws_sqs_queue.s3_event_queue.arn
